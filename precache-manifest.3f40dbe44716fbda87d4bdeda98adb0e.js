97a57e1e42815489423d426596bb8d1d22634a69
deborahtrez <deborahtrez12@gmail.com>
2021-05-05 21:38:48 +0300

    Updates

---
+++ precache-manifest.3f40dbe44716fbda87d4bdeda98adb0e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e3cc963d3d3eec2a925c1032d12ee1",
+    "url": "/React-project2-Todo-app/index.html"
+  },
+  {
+    "revision": "bd7cefccb9fb1eb312f0",
+    "url": "/React-project2-Todo-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a1feec492ee00a00a843",
+    "url": "/React-project2-Todo-app/static/css/main.7932814c.chunk.css"
+  },
+  {
+    "revision": "bd7cefccb9fb1eb312f0",
+    "url": "/React-project2-Todo-app/static/js/2.5b0d2426.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-project2-Todo-app/static/js/2.5b0d2426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1feec492ee00a00a843",
+    "url": "/React-project2-Todo-app/static/js/main.c3cf2214.chunk.js"
+  },
+  {
+    "revision": "77674c7e5f688ce7fa9a",
+    "url": "/React-project2-Todo-app/static/js/runtime-main.a10b73c3.js"
+  },
+  {
+    "revision": "31f926ac134297d62f4ef885ff83c526",
+    "url": "/React-project2-Todo-app/static/media/bg-callout.31f926ac.jpg"
+  },
+  {
+    "revision": "c934a8a6d299bfa55c48589e25982a8c",
+    "url": "/React-project2-Todo-app/static/media/bg-masthead.c934a8a6.jpg"
+  }
+]);
